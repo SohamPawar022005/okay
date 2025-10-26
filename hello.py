332d820c7feb8cf903a6f78ce59a977493aba390
SohamPawar022005 <sohamhpawar022005@gmail.com>
2025-10-26 10:48:14 +0000

    First Python file

---
+++ hello.py
@@ -0,0 +1,17 @@
+# Factorial of a given number
+def factorial(n):
+    if n < 0:
+        return 0  # Factorial is not defined for negative numbers
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        fact = 1
+        while n > 1:
+            fact *= n
+            n -= 1
+        return fact
+
+# Driver Code
+num = 5
+print("Factorial of", num, "is", factorial(num))
+
